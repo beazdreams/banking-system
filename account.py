99ac980b5aa03b206f5fd2049cc9f41d375804fe
Beatriz <beabea@noreply.codeberg.org>
2025-06-06 20:14:34 -0300

    feat: add banking account creation
    Allow the script to register a banking accont for an existing CPF in the user database. An account belongs to only one user, and a user can have multiple accounts

---
+++ account.py
@@ -0,0 +1,55 @@
+"""
+This module contains the methods related to bank account CRUD
+
+Business rules:
+An account contains:
+- agency, identified by the number (default = 0001)
+- account_number
+
+@author: Beatriz (beabea)
+@date: 2025-06-03
+"""
+
+from typing import Any
+from cpf_validator import main as verify_cpf
+
+def register_account(user_arr: list[dict[str, Any]],
+                     acc_arr: list[dict[str, Any]],
+                     agency_number: str="0001") -> list[dict[str, Any]]:
+    """
+    Using a `while` loop, it allows the user to insert a CPF, and, if it's valid,
+    verifies if it's on the user database. If not, refuses the account creation.
+
+    Params:
+    @user_arr: list of users registered in the bank app;
+    @acc_arr: list of accounts existing in the bank;
+    @agency_number: default agency number.
+    """
+
+    while True:
+        account_owner_cpf = input("Insira o CPF do dono da conta a ser criada: ")
+
+        is_valid = verify_cpf(account_owner_cpf)
+
+        if is_valid:
+            print(account_owner_cpf)
+
+        find_user_in_user_db = [user for user in user_arr
+                                if user.get('cpf', None) == account_owner_cpf]
+
+        if len(find_user_in_user_db) == 0:
+            # if a list is returned, then, user exists:
+            raise ValueError('um usuário com este CPF não existe na base de dados.')
+
+        account:dict[str, str|int] = {
+            "id": 1 if acc_arr == [] else len(acc_arr) + 1,
+            "agency": agency_number,
+            "user": account_owner_cpf
+        }
+
+        acc_arr.append(account)
+
+        print(f"Conta de ID {account.get('id', None)} foi criada para "
+            f"o usuário de CPF {account_owner_cpf}")
+
+        return acc_arr
